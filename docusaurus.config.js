225889011e5a8ef1c113fd944b503f1d5b135b54
Jakub Sikora <j.sikora@livechatinc.com>
2022-04-25 13:57:09 +0200

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
             items: [
               {
                 label: "Tutorials",
-                to: "/docs/intro",
+                to: "/",
               },
             ],
           },
